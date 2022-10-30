fa3a733b660cbe3bd4239f4cad15bd2c3e2dbbfb
MD ABDUL HADY <hady.onnorokom@gmail.com>
2022-10-30 15:51:18 +0600

    Linked List covered

--- Learning/SingleLinkedList/src/com/aariyan/cp/Main.java
+++ Learning/SingleLinkedList/src/com/aariyan/cp/Main.java
@@ -6,10 +6,21 @@ public class Main {
         SingleLinkedList linkedList = new SingleLinkedList();
         linkedList.insertAtFirst(1);
         linkedList.insertAtFirst(2);
-        linkedList.insertAfterAnyNode(linkedList.head.next,5);
+        linkedList.insertAfterAnyNode(linkedList.head.next, 5);
         linkedList.insertAtEnd(3);
 
+        System.out.println("Before Delete:");
         linkedList.printLinkedList();
+        System.out.println("After Delete:");
+        linkedList.deleteNode(1);
+        linkedList.printLinkedList();
+
+        System.out.println("After sorting Linked List");
+        linkedList.sortLinkedList(linkedList.head);
+        linkedList.printLinkedList();
+
+        System.out.println("After searching elements");
+        System.out.println(linkedList.isFound(linkedList.head, 7));
     }
 }
 
--- Learning/SingleLinkedList/src/com/aariyan/cp/SingleLinkedList.java
+++ Learning/SingleLinkedList/src/com/aariyan/cp/SingleLinkedList.java
@@ -51,11 +51,71 @@ public class SingleLinkedList {
         return;
     }
 
+    //Deleting a node:
+    public void deleteNode(int position) {
+
+        //if the head is null means no value:
+        if (head == null) {
+            return; //Nothing to delete:
+        }
+        ListNode toDeleteNode = head;
+        if (position == 0) {
+            head = toDeleteNode.next;
+            return;
+        }
+        for (int i = 0; i < position - 1 && toDeleteNode != null; i++) {
+            toDeleteNode = toDeleteNode.next;
+        }
+
+        //Position is not found:
+        if (toDeleteNode == null || toDeleteNode.next == null) {
+            return;
+        }
+
+        //if found then remove the node:
+        ListNode removeFromMemory = toDeleteNode.next;
+        toDeleteNode.next = removeFromMemory.next;
+        removeFromMemory = null;
+
+    }
+
+    public boolean isFound(ListNode head, int valueToFind) {
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            if (currentNode.value == valueToFind) {
+                return true;
+            }
+            currentNode = currentNode.next;
+        }
+        return false;
+    }
+
+    public void sortLinkedList(ListNode head) {
+        ListNode currentNode = head;
+        ListNode nextNode = null;
+        if (head == null) {
+            return;
+        }
+        while (currentNode != null) {
+            nextNode = currentNode.next;
+            while (nextNode != null) {
+                if (currentNode.value > nextNode.value) {
+                    int carry = currentNode.value;
+                    currentNode.value = nextNode.value;
+                    nextNode.value = carry;
+                }
+                nextNode = nextNode.next;
+            }
+            currentNode = currentNode.next;
+        }
+    }
+
     public void printLinkedList() {
         ListNode printList = head;
         while (printList != null) {
             System.out.print(printList.value + " ");
             printList = printList.next;
         }
+        System.out.println();
     }
 }
